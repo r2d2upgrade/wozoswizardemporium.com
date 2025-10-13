1ab2ef0a83f00ce81999c7f0edaac15e6b30f0f7
r2d2upgrade <57727417+r2d2upgrade@users.noreply.github.com>
2025-10-13 21:56:18 +0100

    Add files via upload

---
+++ portfolio/portfolio.js
@@ -0,0 +1,19 @@
+var array_port3DModel = [
+{title:"The Zenith, a sword from Terraria", image:"Zenith", date:"10 Oct 25", tags:"3D model, Prop design, Video games"},
+{title:"Minecraft enchanting table", image:"Minecraft Enchanting", date:"9 Dec 24", tags:"3D model, Prop design, Scene design, Video games"},
+{title:"Team Fortress 2 wallpaper", image:"TF2 Wallpaper", date:"25 Aug 24", tags:"3D model, Scene design, Video games"},
+{title:"Explosives from Terraria", image:"Terraria Explosives Showcase", date:"6 Jul 24", tags:"3D model, Prop design, Video games"},
+{title:"The Space Gun from Terraria", image:"Space Gun Showcase", date:"5 Mar 24", tags:"3D model, Prop design, Video games"},
+{title:"The Ankh Shield from Terraria", image:"Ankh Shield Showcase", date:"31 Dec 23", tags:"3D model, Prop design, Video games"}
+];
+
+var array_portIllustrations = [
+{title:"An angelic lightning knight", image:"Acolyte Fly", date:"26 Apr 25", tags:"Illustration, Character design"},
+{title:"Warhammer 40k character commission", image:"Voidmaster Drift", date:"12 Aug 25", tags:"Illustration, Character design, Commission, 3D Modelling"},
+{title:"Robot character design", image:"Err Robot", date:"11 Jan 25", tags:"Illustration, Character design"},
+{title:"Horseless Headless Horsemann, from Team Fortress 2", image:"HHH", date:"1 Oct 24", tags:"Illustration, Character design, Video games"},
+{title:"Custom pixel art phone icons", image:"Phone Icons", date:"8 Aug 23", tags:"Pixel art, UI design"},
+{title:"Artfight piece of an insectoid warrior", image:"Adelaide Bug", date:"4 Jul 23", tags:"Illustration, Character design"}
+];
+
+console.info("Loaded");
